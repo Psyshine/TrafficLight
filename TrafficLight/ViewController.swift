51fd7548eec29863932854b9b7e5d275be2458fc
Andry Pro <isp.fline@gmail.com>
2020-01-29 14:26:35 +0200

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Andry Pro on 29.01.2020.
+//  Copyright © 2020 Andry Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
